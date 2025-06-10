e65db5628554805889e714b9b480fbf95faa1939
summerdixon <summerdixon2023@g.ucla.edu>
2025-06-09 23:18:29 -0700

    Add essay to About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,9 +2,91 @@ import Footer from '../components/Footer';
 
 export default function About() {
     return (
-    <div>
+      <div>
+    <div className="m-24" style={{ fontFamily: "'Lora', serif" }}>
         
-        <h2>About page</h2>;
+        <h2 className="text-3xl font-bold" style={{ fontFamily: "'DM Mono', monospace" }}>
+          Essay and Works Cited
+        </h2>
+
+        <br></br>
+
+        <div>
+
+        <h3 className="text-center text-xl font-bold">Digital Preservation Sites</h3>
+
+        <br></br>
+
+  <p>
+  Preservation sites serve as immersive archives to physically memorialize the history of a space and its inhabitants. Serving as the heart of the entertainment industry and boasting a diverse, multicultural heritage, it is not surprising that Los Angeles contains many historically significant residences. These sites honor the contributions of famous writers, architects, Hollywood figures, political leaders, and wealthy industrialists. In particular, German exiles such as Thomas Mann, Lion Feuchtwanger, Arnold Schoenberg, Galka Scheyer, and many more immigrated to Los Angeles upon the outbreak of World War II as it offered the opportunity to establish a new creative culture and connect with fellow refugees in the area. This project explores how digital storytelling and interactivity can be used to illuminate exile narratives, specifically through the recreation and interpretation of the domestic spaces of Thomas Mann and Galka Scheyer.
+  </p>
+  
+  <br></br>
+
+  <p>
+  I chose to digitally reconstruct these spaces specifically because they embody the goals of their original inhabitants and provide excellent examples of preservation activism. For Mann, his goal was to spread the message of anti-fascism and peace through his essays and speeches, and the Thomas Mann House currently provides a forum for intellectual debate through its fellowship program and events. Similarly, the Galka Scheyer house now shelters an artist who was displaced by the Palisades Fire, serving to fuel artistic passion as Scheyer had advocated for. The recent histories of these homes further illustrate preservation activism as the actions of the public helped prevent the homes from being torn down or remodeled for private use. The recent preservation of these homes underscores how easily these cultural landmarks could have been lost, reduced to plaques or photographs lacking the spirit of the original. It is important to preserve historic sites such as these because our connections to the past help us become more grounded, informed, and unified. By digitally reconstructing them, we can not only preserve their appearance but also restore their meaning.
+    </p>
+
+    <br></br>
+
+  <p>
+  Before discussing the project and its significance, it is important to understand the background of Thomas Mann and Galka Scheyer in order to humanize and bring to life their exile experiences. Thomas Mann was the most famous German novelist of the 20th century. He notably earned a Nobel Prize for Literature in 1929 and contributed many essays and speeches to the anti-Nazi movement from his new home in Los Angeles. His writings focus on a wide variety of topics such as classism, capitalism, artistry, and ordinary life. During World War I, his work embodied more political themes as he supported the German war effort, unlike his brother Heinrich. When the Weimar Republic was established in 1919, Mann began to realize the shortcomings of authoritarianism and he shifted his writing accordingly. At the time of the Nazi takeover, he was immersed in writing novels and essays that would subtly or overtly encourage citizens to oppose the Nazi cause. On his many tours throughout Europe, Mann expressed his opposition to Nazism and support of Socialist ideologies. When Hitler was elected Chancellor in 1933, Thomas and Katia Mann decided not to return to Germany for some time and lived temporarily in Switzerland, until a trip to the United States persuaded them to settle down there with their six children. The works Mann produced while living in LA reflect the tensions of the war and his political opinions. The most famous of these, Doctor Faustus, relates music, tragedy, and loss of hope to mirror the demise of nations during wartime. As World War II came to a close, anti-leftist attitudes in the United States spurred the Manns to return to Switzerland, where Thomas Mann later died in 1955. 
+  </p>
+
+  <br></br>
+
+  <p>
+  Galka Scheyer was a famous German artist, collector, and dealer. She played a large role in popularizing the group of artists known as the “Blue Four,” consisting of Lyonel Feininger, Alexei Jawlensky, Vassily Kandinsky, and Paul Klee, in the United States. She first met Jawlensky while traveling in Switzerland and subsequently visited the Bauhaus in Weimar where she was introduced to Feininger, Kandinsky, and Klee. Giving them their signature nickname, she took up her role as the group’s legal representative and went about promoting their work in the states at various galleries and exhibitions. She moved out west and settled in Los Angeles after forming a vast professional network, and in 1933, she commissioned Neutra to design her home in which she could display her art and host gatherings. In 1953 after her death, Scheyer’s collection was sent to the Norton Simon Museum in Pasadena, where it resides today.
+  </p>
+
+  <br></br>
+
+  <p>
+    While in exile, Mann and Scheyer left cultural imprints on the city of Los Angeles. From his home on San Remo Drive, Thomas Mann expressed his strong opinions about anti-fascism through overt essays and radio broadcasts, as well as through subtler fictional stories. In his essay The Coming Victory of Democracy, Mann writes, “Throughout the world it has become precarious to take democracy for granted – even in America… America is aware that the time has come for democracy to take stock of itself, for recollection and restatement and conscious consideration, in a word, for its renewal in thought and feeling” (Thomas Mann: Democracy Will Win! Touring Exhibition Concept 2). Through such essays, Mann influenced American intellectual life by linking the country’s ideals with the current fight for justice and freedom. In his “Listen, Germany!” radio broadcast series, Mann implored German citizens to rise up against the hate and violence perpetrated by their Nazi leaders. In his October 1940 speech, he declares, “The question is asked here whether the Germans will not finally recognize that their victories are only steps in an endless quagmire… that there are many better ways to the goal for which we all long: a just peace for all the world” (Mann 6). While these speeches were not directly addressed to the American public, they contributed to the intellectual and moral climate of Los Angeles by turning Mann’s residence into a symbol of resistance. As Tobias Boes’ states in Thomas Mann’s War, “Collectively, these addresses form an important document of Thomas Mann's self-presentation as an author and show how he adapted some of the same rhetorical strategies that had proven effective in the United States for a European audience” (Boes 167). Thomas Mann’s use of advanced rhetoric and staunch promotion of freedom and peace enabled him to resonate with American audiences and displaced Europeans who were in search of a beacon of hope in such a hopeless situation. His home also became an informal salon for other exiled German intellectuals, including Bertolt Brecht and Theodor Adorno, creating a haven for anti-fascist discourse. Similarly, Galka Scheyer also used her Los Angeles residence to foster artistic exchange, showcasing the works of the Blue Four and creating a network of émigré artists so that their work could gain traction with the American public. Scheyer’s promotional work involved more than just gallery shows; she lectured, wrote catalogs, and taught, helping to make the abstract principles of modernist art accessible to American audiences. Together, Mann and Scheyer helped integrate European modernist ideals with the media-centric culture of Los Angeles, allowing them to produce written and visual media and build relationships with fellow émigrés who were also trying to rebuild their lives in the United States.
+  </p>
+
+  <br></br>
+
+  <p>
+    Apart from the intellectual and cultural significance of Mann’s and Scheyer’s stays in Los Angeles, the architecture of their homes serves as a preservation medium and reflects trends in experimental modernism that shaped the architectural identity of the city to this day. Austrian architects Rudolph Schindler and Richard Neutra pioneered the California modernist style and achieved great success throughout Los Angeles, as discussed in Ehrhard Bahr’s Weimar on the Pacific. These two men “manipulated modernism to adapt it to their new home country” (Bahr 151), illustrating how the tension between modernism and the recreation of Old World European ideals in California manifested itself in architecture. The Thomas Mann House blends homeland comfort, modernist ideals, and the distinctive Californian lifestyle, albeit in moderation as the Manns felt that Schindler’s and Neutra’s styles were too progressive. Instead, they enlisted J.R. Davidson as chief architect of their home, who achieved a modernist exterior with clean lines and glass windows while making the interior feel more nostalgic, “comforting them with memories from previous lives” (Herz 20). As Katia Mann said, “So we have a modern house. We like it though” (“History of the Thomas Mann House”). This perfectly captures greater trends of exiles’ struggle to express their identity, as the journey to Los Angeles represented hope for living in a fresh place unmarred by fascism, but it also represented the leaving of all that was familiar in their homeland. In contrast, Galka Scheyer seemed to embrace modernist experimentation more fully, commissioning Neutra to design her home atop the Hollywood Hills. The house, characterized by its boxy design, ribbon windows, and integration with the hillside landscape, functioned as both residence and gallery space. Reinforcing the modernist principle of “form follows function” in the words of famous architect Louis Sullivan, Scheyer’s living spaces were designed with plenty of wall space for her to display her artwork, and as a whole the home was designed to maximize sunlight and provide a vacation-like retreat for her guests. “Conceived as a floating house, the Scheyer House is enthroned like a newly landed airship” (Fuchs 27), fulfilling its purpose as a testament to modernist innovation and a place removed from the bustle of city life. Scheyer’s house physically embodied the Bauhaus values she championed in her promotion of the Blue Four, merging art with everyday life and prioritizing clarity, function, and light.
+  </p>
+
+  <br></br>
+
+  <p>
+  I wanted to memorialize these spaces in my website because as we have seen in recent times, public support for preserving historic residences like those of Thomas Mann and Galka Scheyer has noticeably declined. The historic and cultural value of both homes was largely ignored in their market listings, leaving buyers unaware of the heritage they would be suppressing should they purchase either home. This is surprising and saddening given how much these two individuals shaped the cultural scene in Los Angeles during such a pivotal time in our nation’s history. Not only that, but their homes were designed by famous architects whose work helped shape the current architectural landscape of California today. If not for the actions of the German government and Professor Herz, both homes could have been purchased by private buyers and stripped of their historical meaning. Digital platforms allow for wider access and engagement, facilitating public education about the importance and relevance of such homes. Even as a student at UCLA, I found myself with no knowledge of the rich history of German exile culture in Los Angeles. In my website, I wanted to bring that history to life by creating an interactive space where users can explore various rooms in these homes, learn about their inhabitants’ contributions, and reflect on the value of preservation as both a cultural and political act. Encouraging people to interact with such sites by making them visually engaging, accessible, and informative fosters a deeper appreciation of individual narratives and a wider awareness of exile, inspiring Angelinos and Californians to commit to the preservation of such stories and physical archives.
+  </p>
+
+  <br></br>
+
+  <div>
+  <h3 className="mt-8 mb-4 font-bold text-xl">Works Cited</h3>
+  <ul className="list-disc list-inside space-y-2">
+    <li>Architectural Drawing of the Galka Scheyer House. <em>Art Institute of Chicago</em>, <a href="https://www.artic.edu/artworks/68938/galka-scheyer-house-los-angeles-california-perspective-study" target="_blank" rel="noopener noreferrer">https://www.artic.edu/artworks/68938/galka-scheyer-house-los-angeles-california-perspective-study</a>. Accessed 25 May 2025.</li>
+    <li>Benno Herz [@HerzBenno]. "Standing at the crossroads of history and modernity…" X, 10 May 2024 <em>X</em>, <a href="https://x.com/HerzBenno/status/1787600356861698333" target="_blank" rel="noopener noreferrer">https://x.com/HerzBenno/status/1787600356861698333</a>. Accessed 25 May 2025.</li>
+    <li>Blue Four: The Galka Scheyer Collection. <em>Norton Simon Museum</em>, <a href="https://www.nortonsimon.org/art/in-focus/the-blue-four-galka-scheyer-collection/" target="_blank" rel="noopener noreferrer">https://www.nortonsimon.org/art/in-focus/the-blue-four-galka-scheyer-collection/</a>. Accessed 29 May 2025.</li>
+    <li>Galka Scheyer House as Featured in Architectural Forum, October 1935. <em>ResearchGate</em>, <a href="https://www.researchgate.net/figure/Richard-Neutra-Galka-Scheyer-House-as-featured-in-Architectural-Forum-62-October-1935_fig5_361636456" target="_blank" rel="noopener noreferrer">https://www.researchgate.net/figure/Richard-Neutra-Galka-Scheyer-House-as-featured-in-Architectural-Forum-62-October-1935_fig5_361636456</a>. Accessed 25 May 2025.</li>
+    <li>Galka Scheyer House, Los Angeles, California. <em>Architecture and Design Collection, UC Santa Barbara</em>, <a href="http://www.adc-exhibits.museum.ucsb.edu/items/show/102" target="_blank" rel="noopener noreferrer">http://www.adc-exhibits.museum.ucsb.edu/items/show/102</a>. Accessed 25 May 2025.</li>
+    <li>Galka Scheyer, the Blue Four, and American Modernism. <em>Museo Nacional Thyssen-Bornemisza</em>, <a href="https://www.museothyssen.org/sites/default/files/document/2023-03/Ventanas10_eng_Marcell%C3%A1n_blue%20four.pdf" target="_blank" rel="noopener noreferrer">https://www.museothyssen.org/sites/default/files/document/2023-03/Ventanas10_eng_Marcell%C3%A1n_blue%20four.pdf</a>. Accessed 29 May 2025.</li>
+    <li>History of the Thomas Mann House. <em>Villa Aurora & Thomas Mann House (VATMH)</em>, <a href="https://www.vatmh.org/en/history-162.html" target="_blank" rel="noopener noreferrer">https://www.vatmh.org/en/history-162.html</a>. Accessed 31 May 2025.</li>
+    <li>Knopf, Alfred. “Democracy Will Win.” <em>German History in Documents and Images</em>, <a href="https://ghdi.ghi-dc.org/sub_document.cfm?document_id=2341" target="_blank" rel="noopener noreferrer">https://ghdi.ghi-dc.org/sub_document.cfm?document_id=2341</a>. Accessed 30 May 2025.</li>
+    <li>Modern Art Index Project: Galka Scheyer. <em>The Metropolitan Museum of Art</em>, <a href="https://www.metmuseum.org/research-centers/leonard-a-lauder-research-center/research-resources/modern-art-index-project/scheyer" target="_blank" rel="noopener noreferrer">https://www.metmuseum.org/research-centers/leonard-a-lauder-research-center/research-resources/modern-art-index-project/scheyer</a>. Accessed 30 May 2025.</li>
+    <li>A German Emigré’s Paradise: Thomas Mann in California. <em>TheArticle</em>, <a href="https://www.thearticle.com/a-german-emigres-paradise-thomas-mann-in-california" target="_blank" rel="noopener noreferrer">https://www.thearticle.com/a-german-emigres-paradise-thomas-mann-in-california</a>. Accessed 30 May 2025.</li>
+    <li>Reviving the Echoes of the Past: Augmented Reality in Historical Site Restoration. <em>AR-Locations</em>, <a href="https://ar-locations.com/en/reviving-the-echoes-of-the-past-augmented-reality-in-historical-site-restoration/" target="_blank" rel="noopener noreferrer">https://ar-locations.com/en/reviving-the-echoes-of-the-past-augmented-reality-in-historical-site-restoration/</a>. Accessed 1 June 2025.</li>
+    <li>Thomas Mann. <em>Britannica</em>, <a href="https://www.britannica.com/biography/Thomas-Mann" target="_blank" rel="noopener noreferrer">https://www.britannica.com/biography/Thomas-Mann</a>. Accessed 25 May 2025.</li>
+    <li>Thomas Mann Quotes (II). <em>Notable Quotes</em>, <a href="http://www.notable-quotes.com/m/mann_thomas_ii.html" target="_blank" rel="noopener noreferrer">http://www.notable-quotes.com/m/mann_thomas_ii.html</a>. Accessed 31 May 2025.</li>
+    <li>Thomas Mann. Listen Germany! Twenty-five Radio Messages to the German People over BBC. <em>A.A. Knopf, 1943, pp. 3–6.</em></li>
+    <li>Thomas Mann: Democracy Will Win! Touring Exhibition Concept. <em>Villa Aurora & Thomas Mann House, 2023</em>, <a href="https://www.vatmh.org/de/thomas-mann-democracy-will-win.html?cid=13772&file=files%2Fvilla-aurora%2Ffotos%2Fveranstaltungen%2FProgramme+LA+2023%2FThomas+Mann_Democracy+will+win_concept+%281%29.pdf" target="_blank" rel="noopener noreferrer">https://www.vatmh.org/de/thomas-mann-democracy-will-win.html?cid=13772&file=files%2Fvilla-aurora%2Ffotos%2Fveranstaltungen%2FProgramme+LA+2023%2FThomas+Mann_Democracy+will+win_concept+%281%29.pdf</a>. Accessed 30 May 2025.</li>
+    <li>Thomas Mann’s House. <em>Calisphere</em>, <a href="https://calisphere.org/item/ea9ea8c06b140da72293fc342bac7528/" target="_blank" rel="noopener noreferrer">https://calisphere.org/item/ea9ea8c06b140da72293fc342bac7528/</a>. Accessed 25 May 2025.</li>
+    <li>Wilk, Max. “Will Thomas Mann’s House Be Demolished?” <em>The New Yorker, 27 June 2016</em>, <a href="https://www.newyorker.com/culture/cultural-comment/will-thomas-manns-house-be-demolished" target="_blank" rel="noopener noreferrer">https://www.newyorker.com/culture/cultural-comment/will-thomas-manns-house-be-demolished</a>. Accessed 30 May 2025.</li>
+</ul>
+</div>
+
+
+</div>
+
+</div>
+
 
 
     <div className="p-6"></div>
